57293ba035aaa53ef79d337baba223f64a917016
Dylan Gye <qweroot@pm.me>
2020-11-06 22:49:50 +0900

    init: commit

---
+++ src/layout/full/sidebar/sidebarlinks.js
@@ -0,0 +1,47 @@
+export default [{
+    url: "/dashboard",
+    name: "Dashboard",
+    icon: "mdi mdi-view-dashboard",
+    i18n: "Dashboard",
+    index: 1
+}, {
+    title: "Production Management",
+    i18n: "Production Management",
+}, {
+    url: "/units",
+    name: "Units",
+    icon: "mdi mdi-engine-outline",
+    i18n: "Units",
+    index: 2
+}, {
+    url: "/tasks",
+    name: "Tasks",
+    icon: "mdi mdi-format-list-bulleted-type",
+    i18n: "Tasks",
+    index: 3
+}, {
+    title: "Inventory Management",
+    i18n: "Inventory Management"
+}, {
+    url: "/parts",
+    name: "Pasks",
+    icon: "mdi mdi-nut",
+    i18n: "Pasks",
+    index: 4
+}, {
+    url: "/categories",
+    name: "Categories",
+    icon: "mdi mdi-view-quilt",
+    i18n: "Categories",
+    index: 5
+}, {
+    title: "Order Management",
+    i18n: "Order Management",
+}, {
+    url: "/orders",
+    name: "Orders",
+    icon: "mdi mdi-file-document-box",
+    i18n: "Orders",
+    index: 6
+}
+]
---
+++ src/router.js
@@ -0,0 +1,109 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        // ======================
+        // Blank Layout
+        // ======================    
+        {
+            path: '',
+            component: () => import('@/layout/blank/Blank.vue'),
+            // ======================
+            // Theme routes / pages
+            // ======================
+            children: [{
+                path: '/',
+                redirect: '/login'
+            }, {
+                path: '/login',
+                name: 'Login',
+                index: 1,
+                component: () => import('@/views/authentication/Login.vue')
+            }, {
+                path: '/error404',
+                name: 'Error 404',
+                index: 2,
+                component: () => import('@/views/authentication/Error404.vue')
+            }, {
+                path: '/register',
+                name: 'Register',
+                index: 3,
+                component: () => import('@/views/authentication/Register.vue')
+            }]
+        },
+        {
+            // ======================
+            // Full Layout
+            // ======================
+            path: '',
+            component: () => import('@/layout/full/MainContainer.vue'),
+            // ======================
+            // Theme routes / pages
+            // ======================
+            children: [{
+                path: '/dashboard',
+                name: 'Dashboard',
+                index: 1,
+                i18n: 'Dashboard',
+                component: () => import('@/views/StarterPage.vue')
+            }, {
+                path: '/units',
+                name: 'Units',
+                index: 2,
+                i18n: 'Units',
+                component: () => import('@/views/Units.vue')
+            }, {
+                path: '/tasks',
+                name: 'Tasks',
+                index: 3,
+                i18n: "Tasks",
+                component: () => import('@/views/Tasks.vue')
+            }, {
+                path: '/parts',
+                name: 'Parts',
+                index: 4,
+                i18n: "Parts",
+                component: () => import('@/views/Parts.vue')
+            }, {
+                path: '/categories',
+                name: 'Categories',
+                index: 5,
+                i18n: "Categories",
+                component: () => import('@/views/Categories.vue')
+            }, {
+                path: '/orders',
+                name: 'Orders',
+                index: 6,
+                i18n: "Orders",
+                component: () => import('@/views/Orders.vue')
+            }]
+        },
+        // Redirect to 404 page, if no match found
+        {
+            path: '*',
+            redirect: '/Error404'
+        }
+    ]
+})
+
+import NProgress from 'nprogress';
+
+router.beforeResolve((to, from, next) => {
+    // If this isn't an initial page load.
+    if (to.name) {
+        // Start the route progress bar.
+        NProgress.start()
+    }
+    next()
+})
+
+router.afterEach(() => {
+    // Complete the animation of the route progress bar.
+    NProgress.done()
+})
+export default router
